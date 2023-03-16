c876452b4e506592360fc061b4c2a0a385fdd22b
Saman Eslami Nazari <samaneslami78@gmail.com>
2023-03-16 11:54:33 +0330

    Remove old handlers

--- CA1 - Socket Programming/src/server/view_users_handler.cpp
+++
@@ -1,9 +0,0 @@
-#include "view_users_handler.hpp"
-
-Response ViewUsersHandler::callback(const Request& request) {
-    json response_json = hotel_->getAllUsersInfo();
-    
-    Response response;
-    response.setBody(response_json.dump());
-    return response;
-}
