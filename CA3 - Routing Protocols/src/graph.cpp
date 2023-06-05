fc9576a96e136f7d6d5adcf9233562754d6419fb
Saman Eslami Nazari <samaneslami78@gmail.com>
2023-06-05 15:00:38 +0330

    Add toString method to Graph

--- CA3 - Routing Protocols/src/graph.cpp
+++ CA3 - Routing Protocols/src/graph.cpp
@@ -1,4 +1,8 @@
 #include <graph.hpp>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
 
 Graph::Graph(int nodes_count)
     : adj_vec_(nodes_count, std::vector<Graph::Link>(nodes_count)) {}
@@ -21,4 +25,23 @@ int Graph::getCost(int src, int dest) const {
 }
 
 std::string Graph::toString() const {
+    std::ostringstream output_str;
+    output_str << std::setw(5) << " ";
+    output_str << "|";
+    for (int i = 1; i <= adj_vec_.size(); ++i)
+        output_str << std::setw(5) << i;
+    output_str << std::endl;
+    for (int i = 1; i <= adj_vec_.size(); ++i) {
+        output_str << std::setw(5) << i;
+        output_str << "|";
+        for (int j = 1; j <= adj_vec_.size(); ++j) {
+            if (i == j) {
+                output_str << std::setw(5) << 0;
+                continue;
+            }
+            output_str << std::setw(5) << adj_vec_[i - 1][j - 1].cost_;
+        }
+        output_str << std::endl;
+    }
+    return output_str.str();
 }
